68b2eb82ff2d387607104aa9b6399de287cf58c4
Megan Jones <115540207+megj88@users.noreply.github.com>
2023-03-12 11:03:45 +0100

    add cv

--- src/components/Header/CT.jsx
+++ src/components/Header/CT.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./header.css"
+import CV from "../../Assets/Megan Jones CV.pdf"
 
 const CT = () => {
   return (
     <div className="ct">
-        Download CV
+        <a href={CV} download="newfilename" className="btn btn-primary">Download CV</a>
         <a href="#contact" className="btn btn-primary">Let's talk</a>
       
     </div>
